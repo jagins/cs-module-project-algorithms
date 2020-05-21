a32ea06cd35671cc00dde3a8bd193db400d4ddb5
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-21 18:23:01 -0500

    moving zeros works over complicated it

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,21 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    #input = [0, 3, 1, 0, -2]
+    #output = [3, 1, -2, 0, 0]
 
-    pass
+    #step 1 = loop through the array
+    #step 2 = check if current element is 0 and next element is greater than 0
+        #swap if true
+    temp = 0
+    for i in range(0,len(arr) - 1):
+        if arr[i] == 0:
+            # temp = arr[i]
+            # arr[i] = arr[i + 1]
+            # arr[i + 1] =  temp
+            arr.append(arr.pop(i))
 
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
